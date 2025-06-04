e0bdeda7ce2e35f7a10cef5d73ee157488d673eb
Oscar Adolfo Gonzalez Aballay <oscar_aballay246@hotmail.com>
2025-06-03 22:02:29 -0300

    Ejercicio resuelto

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
 //Arboles binarios prodevs ejercicio de practica logica
 
 //Árbol Binario de Búsqueda (Binary Search Tree)
-//Implementá una clase BinarySearchTree que represente un Árbol Binario de Búsqueda. Este árbol debe construirse utilizando recursión y debe cumplir con las siguientes funcionalidades:
+//Implementá una clase BinarySearchTree que represente un Árbol Binario de Búsqueda.
+// Este árbol debe construirse utilizando recursión y debe cumplir con las siguientes funcionalidades:
 // Métodos obligatorios:
 //insert(value)
 //Inserta un nuevo valor en el árbol respetando la propiedad de los BST:
@@ -45,8 +46,63 @@ class BinarySearchTree {
     this.right = null;
   }
 
+  insert(value) {
+    if (value < this.value) {
+      if (this.left) {
+        this.left.insert(value);
+      } else {
+        this.left = new BinarySearchTree(value);
+      }
+    } else {
+      if (this.right) {
+        this.right.insert(value);
+      } else {
+        this.right = new BinarySearchTree(value);
+      }
+    }
+  }
 
+  contains(value) {
+    if (this.value === value) return true;
+    if (value < this.value) {
+      return this.left ? this.left.contains(value) : false;
+    } else {
+      return this.right ? this.right.contains(value) : false;
+    }
+  }
+
+  size() {
+    let leftSize = this.left ? this.left.size() : 0;
+    let rightSize = this.right ? this.right.size() : 0;
+    return 1 + leftSize + rightSize;
+  }
+
+  depthFirstForEach(cb, order = "in-order") {
+    if (order === "pre-order") {
+      cb(this.value);
+      if (this.left) this.left.depthFirstForEach(cb, order);
+      if (this.right) this.right.depthFirstForEach(cb, order);
+    } else if (order === "post-order") {
+      if (this.left) this.left.depthFirstForEach(cb, order);
+      if (this.right) this.right.depthFirstForEach(cb, order);
+      cb(this.value);
+    } else { // in-order
+      if (this.left) this.left.depthFirstForEach(cb, order);
+      cb(this.value);
+      if (this.right) this.right.depthFirstForEach(cb, order);
+    }
+  }
+
+  breadthFirstForEach(cb, queue = []) {
+    cb(this.value);
+    if (this.left) queue.push(this.left);
+    if (this.right) queue.push(this.right);
+    if (queue.length > 0) {
+      queue.shift().breadthFirstForEach(cb, queue);
+    }
+  }
 }
+
 
 
 // No modifiquen nada debajo de esta linea
